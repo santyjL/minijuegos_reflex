7470fb5426589009495f088d7c5cd376ed3c25d9
santyjL <jlopezsanty1@gmail.com>
2024-02-06 11:40:51 -0600

    nada nuevo

---
+++ .web/pages/principal.js
@@ -0,0 +1,131 @@
+/** @jsxImportSource @emotion/react */
+
+
+import { Fragment } from "react"
+import { Fragment_fd0e7cb8f9fb4669a6805377d925fba0 } from "/utils/stateful_components"
+import { Box, Center, Heading, HStack, Image as ChakraImage, Link, Spacer, Text, VStack } from "@chakra-ui/react"
+import NextLink from "next/link"
+import "focus-visible/dist/focus-visible"
+import NextHead from "next/head"
+
+
+
+export default function Component() {
+
+  return (
+    <Fragment>
+  <Fragment_fd0e7cb8f9fb4669a6805377d925fba0/>
+  <Box sx={{"bg": "#292833", "backgroundSize": "cover", "height": "100vh"}}>
+  <Box sx={{"bg": "#FF5C00", "border": "1px solid #000"}}>
+  <HStack>
+  <Link as={NextLink} href={`principal`}>
+  <HStack>
+  <Heading size={`lg`} sx={{"color": "#000000"}}>
+  {`Game`}
+</Heading>
+  <Heading size={`lg`} sx={{"color": "#FFFFFF"}}>
+  {`Mini`}
+</Heading>
+  <Heading size={`lg`} sx={{"color": "#FFFFFF"}}>
+  {`⭐`}
+</Heading>
+</HStack>
+</Link>
+  <Spacer/>
+  <Link as={NextLink} href={`https://github.com/santyjL/santyjL`} isExternal={true}>
+  <ChakraImage src={`/github icon.png`} sx={{"width": "3.6em", "height": "100%"}}/>
+</Link>
+</HStack>
+</Box>
+  <Center>
+  <Box sx={{"borderRadius": "0.9em", "background": "#32135A", "margin": "3em", "padding": "0.8", "border": "1px solid #000", "boxShadow": "2px 2px 2px 0px #FF5C00", "width": "40%"}}>
+  <Center>
+  <Heading sx={{"color": "#FFFFFF", "fontSize": "2.7em"}}>
+  {`Mini Juegos disponibles`}
+</Heading>
+</Center>
+</Box>
+</Center>
+  <Box sx={{"width": "100%"}}>
+  <Center>
+  <VStack>
+  <Center>
+  <HStack>
+  <Box sx={{"width": "69%"}}>
+  <Center sx={{"borderRadius": "0.9em", "background": "#32135A", "marginX": "1.5em", "marginY": "1em", "padding": "0.8", "border": "1px solid #000", "boxShadow": "2px 2px 2px 0px #FF5C00", "width": null}}>
+  <VStack sx={{"marginX": "1.5em", "marginY": "1em"}}>
+  <Link as={NextLink} href={`reglas_juego_uno`}>
+  <Heading sx={{"color": "#FFFFFF", "fontSize": "2.7em"}}>
+  {`Piedra🥌 , Papel📋 , Tijeras✂ , Lagarto🦎 , spock 🖖`}
+</Heading>
+</Link>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`El clasico juego de pieda papel o tijeras lo conocemos todo el mundo pero no todos conocen el juego de piedra,papel,tijeras,gallina,spock`}
+</Text>
+</VStack>
+</Center>
+</Box>
+  <Spacer/>
+  <Box sx={{"width": "30%"}}>
+  <Center sx={{"borderRadius": "0.9em", "background": "#32135A", "marginX": "1.5em", "marginY": "1em", "padding": "0.8", "border": "1px solid #000", "boxShadow": "2px 2px 2px 0px #FF5C00", "width": null}}>
+  <VStack sx={{"marginX": "1.5em", "marginY": "1em"}}>
+  <Link as={NextLink} href={`Ping_pong`}>
+  <Heading sx={{"color": "#FFFFFF", "fontSize": "2.7em"}}>
+  {`Ping Pong 🏓|🏓`}
+</Heading>
+</Link>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`El ping pog,unos de los primeros juegos existente en el mundo.`}
+</Text>
+</VStack>
+</Center>
+</Box>
+</HStack>
+</Center>
+  <Center sx={{"width": "100%"}}>
+  <HStack>
+  <Box sx={{"width": "100%"}}>
+  <Center sx={{"borderRadius": "0.9em", "background": "#32135A", "marginX": "1.5em", "marginY": "1em", "padding": "0.8", "border": "1px solid #000", "boxShadow": "2px 2px 2px 0px #FF5C00", "width": "92.4%"}}>
+  <VStack sx={{"marginX": "1.5em", "marginY": "1em"}}>
+  <Link as={NextLink} href={`Encuentra_el_numero`}>
+  <Heading sx={{"color": "#FFFFFF", "fontSize": "2.7em"}}>
+  {`Encuentra el numero 🎲`}
+</Heading>
+</Link>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`del 1 al 100 se a perdido un numero pero cual sera, hay que entcontrarlo`}
+</Text>
+</VStack>
+</Center>
+</Box>
+  <Spacer/>
+  <Box sx={{"width": "100%"}}>
+  <Center sx={{"borderRadius": "0.9em", "background": "#32135A", "marginX": "1.5em", "marginY": "1em", "padding": "0.8", "border": "1px solid #000", "boxShadow": "2px 2px 2px 0px #FF5C00", "width": "92.4%"}}>
+  <VStack sx={{"marginX": "1.5em", "marginY": "1em"}}>
+  <Link as={NextLink} href={`3_en_rayas`}>
+  <Heading sx={{"color": "#FFFFFF", "fontSize": "2.7em"}}>
+  {`Tres en raya ❌ | ⭕  `}
+</Heading>
+</Link>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`El clasico “Tres en raya no podia faltar  un juego mitico que todos conocemos`}
+</Text>
+</VStack>
+</Center>
+</Box>
+</HStack>
+</Center>
+</VStack>
+</Center>
+</Box>
+</Box>
+  <NextHead>
+  <title>
+  {`Reflex App`}
+</title>
+  <meta content={`A Reflex app.`} name={`description`}/>
+  <meta content={`favicon.ico`} property={`og:image`}/>
+</NextHead>
+</Fragment>
+  )
+}
---
+++ .web/pages/reglas_juego_uno.js
@@ -0,0 +1,152 @@
+/** @jsxImportSource @emotion/react */
+
+
+import { Fragment } from "react"
+import { Fragment_fd0e7cb8f9fb4669a6805377d925fba0 } from "/utils/stateful_components"
+import { Box, Button, Center, Heading, HStack, Image as ChakraImage, Link, Spacer, Text, VStack } from "@chakra-ui/react"
+import NextLink from "next/link"
+import "focus-visible/dist/focus-visible"
+import NextHead from "next/head"
+
+
+
+export default function Component() {
+
+  return (
+    <Fragment>
+  <Fragment_fd0e7cb8f9fb4669a6805377d925fba0/>
+  <Box sx={{"bg": "#292833", "backgroundSize": "cover", "height": "100vh"}}>
+  <Box sx={{"bg": "#FF5C00", "border": "1px solid #000"}}>
+  <HStack>
+  <Link as={NextLink} href={`principal`}>
+  <HStack>
+  <Heading size={`lg`} sx={{"color": "#000000"}}>
+  {`Game`}
+</Heading>
+  <Heading size={`lg`} sx={{"color": "#FFFFFF"}}>
+  {`Mini`}
+</Heading>
+  <Heading size={`lg`} sx={{"color": "#FFFFFF"}}>
+  {`⭐`}
+</Heading>
+</HStack>
+</Link>
+  <Spacer/>
+  <Link as={NextLink} href={`https://github.com/santyjL/santyjL`} isExternal={true}>
+  <ChakraImage src={`/github icon.png`} sx={{"width": "3.6em", "height": "100%"}}/>
+</Link>
+</HStack>
+</Box>
+  <Box sx={{"width": "100%"}}>
+  <Center sx={{"borderRadius": "0.9em", "background": "#32135A", "marginX": "1.5em", "marginY": "1em", "padding": "0.8", "border": "1px solid #000", "boxShadow": "2px 2px 2px 0px #FF5C00", "width": null}}>
+  <VStack>
+  <HStack>
+  <Heading sx={{"color": "#FFFFFF", "fontSize": "2.7em"}}>
+  {`Piedra🥌 , Papel📋 , Tijeras✂ , Lagarto🦎 , Spock 🖖`}
+</Heading>
+</HStack>
+  <HStack>
+  <Box sx={{"textAlign": "left"}}>
+  <HStack>
+  <Text sx={{"color": "#FFFFFF", "fontSize": "1.4em"}}>
+  {`1.Piedra aplasta Tijeras : `}
+</Text>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`La Piedra gana porque puede romper las Tijeras.`}
+</Text>
+</HStack>
+  <HStack>
+  <Text sx={{"color": "#FFFFFF", "fontSize": "1.4em"}}>
+  {`2.Tijeras cortan Papel : `}
+</Text>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`Las Tijeras ganan al cortar el Papel.`}
+</Text>
+</HStack>
+  <HStack>
+  <Text sx={{"color": "#FFFFFF", "fontSize": "1.4em"}}>
+  {`3.Papel cubre Piedra : `}
+</Text>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`El Papel gana al cubrir la Piedra. `}
+</Text>
+</HStack>
+  <HStack>
+  <Text sx={{"color": "#FFFFFF", "fontSize": "1.4em"}}>
+  {`4.Piedra aplasta Lagarto : `}
+</Text>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`La Piedra gana al aplastar al Lagarto.`}
+</Text>
+</HStack>
+  <HStack>
+  <Text sx={{"color": "#FFFFFF", "fontSize": "1.4em"}}>
+  {`5.Lagarto envenena Spock :  `}
+</Text>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`El Lagarto gana al envenenar a Spock.`}
+</Text>
+</HStack>
+  <HStack>
+  <Text sx={{"color": "#FFFFFF", "fontSize": "1.4em"}}>
+  {`6.Spock destroza Tijeras:`}
+</Text>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`Spock gana al destrozar las Tijeras.`}
+</Text>
+</HStack>
+  <HStack>
+  <Text sx={{"color": "#FFFFFF", "fontSize": "1.4em"}}>
+  {`7.Tijeras decapitan Lagarto : `}
+</Text>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`Las Tijeras ganan al decapitar al Lagarto.`}
+</Text>
+</HStack>
+  <HStack>
+  <Text sx={{"color": "#FFFFFF", "fontSize": "1.4em"}}>
+  {`8.Lagarto come Papel : `}
+</Text>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`El Lagarto gana al comer el Papel.`}
+</Text>
+</HStack>
+  <HStack>
+  <Text sx={{"color": "#FFFFFF", "fontSize": "1.4em"}}>
+  {`9.Papel desautoriza Spock : `}
+</Text>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`El Papel gana al desautorizar a Spock.`}
+</Text>
+</HStack>
+  <HStack>
+  <Text sx={{"color": "#FFFFFF", "fontSize": "1.4em"}}>
+  {`10.Spock vaporiza Piedra:`}
+</Text>
+  <Text sx={{"color": "#C1C1C1", "fontSize": "1.4em"}}>
+  {`Spock gana al vaporizar la Piedra.`}
+</Text>
+</HStack>
+</Box>
+</HStack>
+</VStack>
+</Center>
+</Box>
+  <Button sx={{"borderRadius": "0.9em", "background": "#FF5C00", "border": "1px solid #000", "boxShadow": "2px 2px 2px 0px #32135A", "marginLeft": "75%"}}>
+  <Link as={NextLink} href={`3_en_rayas`}>
+  <Text sx={{"fontSize": "2.7em"}}>
+  {`continuar`}
+</Text>
+</Link>
+</Button>
+</Box>
+  <NextHead>
+  <title>
+  {`Reflex App`}
+</title>
+  <meta content={`A Reflex app.`} name={`description`}/>
+  <meta content={`favicon.ico`} property={`og:image`}/>
+</NextHead>
+</Fragment>
+  )
+}
